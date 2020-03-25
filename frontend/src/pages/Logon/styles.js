bc1d3f7afbff6ec59948a1faa8464dc3cce3dd43
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-03-25 16:56:45 -0300

    fix: Correção espaçamento login

--- frontend/src/pages/Logon/styles.js
+++ frontend/src/pages/Logon/styles.js
@@ -8,7 +8,7 @@ export const Container = styled.div`
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 `;
 
 export const FormArea = styled.section`
