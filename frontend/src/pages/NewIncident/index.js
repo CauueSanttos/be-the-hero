bd155ee4a4c6a5c7edc2c30cf7a33c4754e1955c
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-03-25 22:24:26 -0300

    feat: Criado o cadastro de Casos

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '~/services/api';
 
 import TextInput from '~/components/TextInput';
 import TextArea from '~/components/TextArea';
@@ -11,6 +15,34 @@ import { Container, Content, Description, Form } from './styles';
 import logoImg from '~/assets/logo.svg';
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ong_id = localStorage.getItem('bethehero@ongId');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = { title, description, value };
+
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ong_id,
+        },
+      });
+
+      history.push('/profile');
+
+      toast.success('Caso cadastrado com sucesso!');
+    } catch (err) {
+      toast.error('Ocorreu um erro ao incluir, tente novamente!');
+    }
+  }
+
   return (
     <Container>
       <Content>
@@ -29,10 +61,23 @@ export default function NewIncident() {
           </FormLink>
         </Description>
 
-        <Form>
-          <TextInput placeholder="Título do caso" />
-          <TextArea textarea placeholder="Descrição" />
-          <TextInput placeholder="Valor em reais" />
+        <Form onSubmit={handleSubmit}>
+          <TextInput
+            placeholder="Título do caso"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextArea
+            textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <TextInput
+            placeholder="Valor em reais"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <Button type="submit">Cadastrar</Button>
         </Form>
